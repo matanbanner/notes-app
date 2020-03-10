4bddfb23901e25af80110f79029c0f8366761767
Matan Banner <matan.banner@gmail.com>
2020-03-10 12:26:07 +0200

    comment was removed and git ignore note.json

--- notes.js
+++ notes.js
@@ -5,8 +5,6 @@ const addNote = function(title, body) {
     const notes = loadNotes()
     const duplicateNote = notes.find( (note) => note.title === title)
 
-    // debugger
-
     if (!duplicateNote) {
         notes.push({
             title: title,
